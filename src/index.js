8dd6fecf6d903e8ecd44fb6bbc230e983d93c1d6
Mason Stallmo <mstallmo@stats.com>
2017-10-25 15:25:03 -0500

    Add and connect Redux in application

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/confgureStore';
+import {Provider} from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render ((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>), document.getElementById('app')
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>), document.getElementById('app')
 );
